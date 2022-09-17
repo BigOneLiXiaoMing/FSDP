de7255f3819cd57075a34992f46eb47ae935c204
BigOneLiXiaoMing <99854690+BigOneLiXiaoMing@users.noreply.github.com>
2022-09-17 15:35:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,33 +1,48 @@
 ## main.py文件
-from torch.nn.parallel import DistributedDataParallel as DDP
 import torch
-# 新增：
-import torch.distributed as dist
-import torch.nn as nn
-
-# 新增：从外面得到local_rank参数
 import argparse
+
+# 新增1:依赖
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+# 新增2：从外面得到local_rank参数，在调用DDP的时候，其会自动给出这个参数，后面还会介绍。所以不用考虑太多，照着抄就是了。
+#       argparse是python的一个系统库，用来处理命令行调用，如果不熟悉，可以稍微百度一下，很简单！
 parser = argparse.ArgumentParser()
 parser.add_argument("--local_rank", default=-1)
 FLAGS = parser.parse_args()
 local_rank = FLAGS.local_rank
 
-# 新增：DDP backend初始化
-torch.cuda.set_device('cuda:'+local_rank)
-dist.init_process_group(backend='nccl')  # nccl是GPU设备上最快、最推荐的后端
+# 新增3：DDP backend初始化
+#   a.根据local_rank来设定当前使用哪块GPU
+torch.cuda.set_device(local_rank)
+#   b.初始化DDP，使用默认backend(nccl)就行。如果是CPU模型运行，需要选择其他后端。
+dist.init_process_group(backend='nccl')
 
-# 构造模型
-# device = torch.device("cuda", local_rank)
-device = torch.device("cuda:"+local_rank)
+# 新增4：定义并把模型放置到单独的GPU上，需要在调用`model=DDP(model)`前做哦。
+#       如果要加载模型，也必须在这里做哦。
+device = torch.device("cuda", local_rank)
 model = nn.Linear(10, 10).to(device)
-# 新增：构造DDP model
-model = DDP(model, device_ids=[int(local_rank)], output_device=int(local_rank))
+# 可能的load模型...
 
-# 前向传播
-outputs = model(torch.randn(20, 10).to(rank))
-labels = torch.randn(20, 10).to(rank)
-loss_fn = nn.MSELoss()
-loss_fn(outputs, labels).backward()
-# 后向传播
-optimizer = optim.SGD(model.parameters(), lr=0.001)
-optimizer.step()
+# 新增5：之后才是初始化DDP模型
+model = DDP(model, device_ids=[local_rank], output_device=local_rank)
+
+my_trainset = torchvision.datasets.CIFAR10(root='./data', train=True)
+# 新增1：使用DistributedSampler，DDP帮我们把细节都封装起来了。用，就完事儿！
+#       sampler的原理，后面也会介绍。
+train_sampler = torch.utils.data.distributed.DistributedSampler(my_trainset)
+# 需要注意的是，这里的batch_size指的是每个进程下的batch_size。也就是说，总batch_size是这里的batch_size再乘以并行数(world_size)。
+trainloader = torch.utils.data.DataLoader(my_trainset, batch_size=batch_size, sampler=train_sampler)
+
+
+for epoch in range(num_epochs):
+    # 新增2：设置sampler的epoch，DistributedSampler需要这个来维持各个进程之间的相同随机数种子
+    trainloader.sampler.set_epoch(epoch)
+    # 后面这部分，则与原来完全一致了。
+    for data, label in trainloader:
+        prediction = model(data)
+        loss = loss_fn(prediction, label)
+        loss.backward()
+        optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+        optimizer.step()
