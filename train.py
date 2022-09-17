9a11135964f3876f488dacadc2437136b5e0efb1
BigOneLiXiaoMing <99854690+BigOneLiXiaoMing@users.noreply.github.com>
2022-09-17 16:06:58 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import torch
 import argparse
 import torch.nn as nn
 import torchvision
+import torch.nn.functional as F
+import torch.optim as optim
 
 # 新增1:依赖
 import torch.distributed as dist
@@ -39,16 +41,17 @@ train_sampler = torch.utils.data.distributed.DistributedSampler(my_trainset)
 # 需要注意的是，这里的batch_size指的是每个进程下的batch_size。也就是说，总batch_size是这里的batch_size再乘以并行数(world_size)。
 trainloader = torch.utils.data.DataLoader(my_trainset, batch_size=50, sampler=train_sampler) # 此处黄子昱随便设置了batch-size。。。
 
+optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 for epoch in range(100):
     # 新增2：设置sampler的epoch，DistributedSampler需要这个来维持各个进程之间的相同随机数种子
     trainloader.sampler.set_epoch(epoch)
     # 后面这部分，则与原来完全一致了。
-    for data, label in trainloader:
+    for batch_idx, (data, label) in enumerate(trainloader):
+        data, label = data.device("cuda:"+local_rank), label.device("cuda:"+local_rank)
         prediction = model(data)
-        loss = loss_fn(prediction, label)
+        loss = F.nll_loss(prediction, label)
         loss.backward()
-        optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
         optimizer.step()
 # 1. save模型的时候，和DP模式一样，有一个需要注意的点：保存的是model.module而不是model。
 #    因为model其实是DDP model，参数是被`model=DDP(model)`包起来的。
